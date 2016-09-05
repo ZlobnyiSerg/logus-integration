2748a48b70a7d9a4cf929d9fe094fa0b2da320fd
Sergey Zhizhenko <zlobnyiserg@gmail.com>
2016-09-05 17:42:27 +0300

    Обновлена ссылка на общую сборку сообщений. Добавлен пример обработки сообщений об изменении наличия

---
+++ LogusBridge/Handlers/AvailabilityChangedHandler.cs
@@ -0,0 +1,19 @@
+﻿using Logus.HMS.Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace LogusBridge.Handlers
+{
+    /// <summary>
+    ///     Обработчик сообщения об изменении в брони
+    /// </summary>
+    public class AvailabilityChangedHandler : IHandleMessages<AvailabilityChangedEvent>
+    {
+        protected static readonly ILog Log = LogManager.GetLogger<AvailabilityChangedHandler>();        
+
+        public void Handle(AvailabilityChangedEvent message)
+        {
+            Log.InfoFormat($"Получено сообщение об изменении наличия в отеле {message.PropertyId}, на даты {message.DateRange}, тип комнаты '{message.RoomTypeCode}'");
+        }
+    }
+}
